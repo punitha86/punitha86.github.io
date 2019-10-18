283277e02ced278a7347cbeda54d3e51a3b15b51
punitha86 <punith.job@gmail.com>
2019-10-18 15:51:05 -0700

    enabling click to call news ajax

--- js/app.js
+++ js/app.js
@@ -1,3 +1,70 @@
+
+
+const newsAjaxCall = () => {
+//creating container for teh news articles
+// <div class="container">
+$('.container').empty();
+$('.container').append($('<h1>').text('NEWS'));
+$('.container').append($('<div>').addClass('content'));
+$('.content').append($('<div>').addClass('news-carousel-button previous'));
+$('.news-carousel-button previous').append($('<span class="lnr lnr-chevron-left"></span>'))
+$('.content').append($('<div>').addClass('news-carousel-images'));
+$('.content').append($('<div>').addClass('news-carousel-button next'));
+$('.news-carousel-button next').append($('<span class="lnr lnr-chevron-right"></span>'))
+
+
+  $.ajax({
+    url: "https://newsapi.org/v2/top-headlines?country=us&apiKey=c9b28a91c2114ad2bb5aed577298b412"
+  }).then((data) => {
+    for (x of data.articles) {
+      $('.news-carousel-images').append($('<img>').attr('src', x.urlToImage).addClass('newsimg'));
+      $('.news-carousel-images').append($('<p>').text(x.title));
+      $('.news-carousel-images').append($('<article>').text(x.description).addClass('description'));
+    } //end of for loop
+////on click of the images we need to give out its conetent
+    // $('.newsimg').on('click',() => {
+    //   $(event.target).siblings('.description').eq(0).css('display','block');
+    //   //$('.content').append('<p>'+x.description+'</p>');
+    // })
+
+    //////////////writing the carousel logic///////////////
+    let currentImgIndex = 0;
+    let highestIndex = $('.news-carousel-images').children().length - 1;
+
+    $('.next').on('click', () => {
+
+      /////first moving to the next element/////////
+      //hiding the first image//////
+      $('.news-carousel-images').children().eq(currentImgIndex).css('display', 'none');
+      ////hiding the description of the first image
+      $('.news-carousel-images').children().eq(currentImgIndex + 1).css('display', 'none');
+      //moving the index to the next image and check for last image & loop
+      (currentImgIndex < highestIndex) ? currentImgIndex +=3 : currentImgIndex = 0;
+
+      //bringing up the next image and description//
+      $('.news-carousel-images').children().eq(currentImgIndex).css('display', 'block');
+      $('.news-carousel-images').children().eq(currentImgIndex + 1).css('display', 'block');
+    });
+
+/////first moving to the previous element/////////
+    //hiding current element
+    $('.previous').on('click', () => {
+      $('.news-carousel-images').children().eq(currentImgIndex).css('display', 'none');
+      $('.news-carousel-images').children().eq(currentImgIndex + 1).css('display', 'none');
+      //moving the index to the previous image and check for first img & loop
+      (currentImgIndex > 0)? currentImgIndex -= 3: currentImgIndex = highestIndex;
+      //bringing up the previous image and description//
+      $('.news-carousel-images').children().eq(currentImgIndex).css('display', 'block');
+      $('.news-carousel-images').children().eq(currentImgIndex + 1).css('display', 'block');
+    })
+
+
+  }, (error) => {
+
+  })
+}
+
+
 $(() => {
   //console.log("App.js successfully connected");
   //Calling weather api to check for its values
@@ -15,59 +82,17 @@ $(() => {
   // return num;
   //     }
   //
-  //     $('.content').append($('<p>').text("Temperature : " + convertToFahrenheit(data.main.temp)));
-  //     $('.content').append($('<p>').text("Pressure : " + data.main.pressure));
-  //     $('.content').append($('<p>').text("Humidity : " + data.main.humidity));
-  //     $('.content').append($('<p>').text("Min-Temp : " + convertToFahrenheit(data.main.temp_min)));
-  //     $('.content').append($('<p>').text("Max-Temp : " + convertToFahrenheit(data.main.temp_max)));
+  //     $('.container').append($('<p>').text("Temperature : " + convertToFahrenheit(data.main.temp)));
+  //     $('.container').append($('<p>').text("Pressure : " + data.main.pressure));
+  //     $('.container').append($('<p>').text("Humidity : " + data.main.humidity));
+  //     $('.container').append($('<p>').text("Min-Temp : " + convertToFahrenheit(data.main.temp_min)));
+  //     $('.container').append($('<p>').text("Max-Temp : " + convertToFahrenheit(data.main.temp_max)));
   //     //console.log(data.main.temp);
   //   }, (error) => {
   //     console.log(error);
   //   })
 
-  $.ajax({
-    url: "https://newsapi.org/v2/top-headlines?country=us&apiKey=c9b28a91c2114ad2bb5aed577298b412"
-  }).then((data) => {
-    for (x of data.articles) {
-      $('.news-carousel-images').append($('<img>').attr('src', x.urlToImage).addClass('newsimg'));
-      $('.news-carousel-images').append($('<p>').text(x.title).addClass('description'));
-    } //end of for loop
-
-    //////////////writing the carousel logic///////////////
-    let currentImgIndex = 0;
-    let highestIndex = $('.news-carousel-images').children().length - 1;
-
-    $('.next').on('click', () => {
-
-      /////first moving to the next element/////////
-      //hiding the first image//////
-      $('.news-carousel-images').children().eq(currentImgIndex).css('display', 'none');
-      ////hiding the description of the first image
-      $('.news-carousel-images').children().eq(currentImgIndex + 1).css('display', 'none');
-      //moving the index to the next image and check for last image & loop
-      (currentImgIndex < highestIndex) ? currentImgIndex +=2 : currentImgIndex = 0;
-
-      //bringing up the next image and description//
-      $('.news-carousel-images').children().eq(currentImgIndex).css('display', 'block');
-      $('.news-carousel-images').children().eq(currentImgIndex + 1).css('display', 'block');
-    });
-
-/////first moving to the previous element/////////
-    //hiding current element
-    $('.previous').on('click', () => {
-      $('.news-carousel-images').children().eq(currentImgIndex).css('display', 'none');
-      $('.news-carousel-images').children().eq(currentImgIndex + 1).css('display', 'none');
-      //moving the index to the previous image and check for first img & loop
-      (currentImgIndex > 0)? currentImgIndex -= 2: currentImgIndex = highestIndex;
-      //bringing up the previous image and description//
-      $('.news-carousel-images').children().eq(currentImgIndex).css('display', 'block');
-      $('.news-carousel-images').children().eq(currentImgIndex + 1).css('display', 'block');
-    })
-
-
-  }, (error) => {
-
-  })
+$('#news').on('click',newsAjaxCall)
 
 
 })
