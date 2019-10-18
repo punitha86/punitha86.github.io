1bf25a5e92195ed1e6990b770c9cee4248439a8b
punitha86 <punith.job@gmail.com>
2019-10-18 11:46:45 -0700

    adding the weather API

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # punitha86.github.io
 Morning Rituals
+This site is aimed to show all that u need for a morning. "Morning Rituals" page which will have News, weather, to-do list, some social media links, and am planning to do the news as a image carousel. There will be a fun game for commuting too.
---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(() => {
+  //console.log("App.js successfully connected");
+  //Calling weather api to check for its values
+  //c4a833b10b9fb7fdc0ba57f9b6a5e4a3
+  //api.openweathermap.org/data/2.5/weather?zip={zip code}
+  //news key: c9b28a91c2114ad2bb5aed577298b412
+
+  
+////weather api giving out the weather information
+  $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather?zip=94087&appid=c4a833b10b9fb7fdc0ba57f9b6a5e4a3"
+  }).then((data) => {
+    const convertToFahrenheit = (num) => {
+      num =parseFloat((num-273.15) * (9/5) + 32).toFixed(1);
+return num;
+    }
+
+    $('.content').append($('<p>').text("Temperature : " + convertToFahrenheit(data.main.temp)));
+    $('.content').append($('<p>').text("Pressure : " + data.main.pressure));
+    $('.content').append($('<p>').text("Humidity : " + data.main.humidity));
+    $('.content').append($('<p>').text("Min-Temp : " + convertToFahrenheit(data.main.temp_min)));
+    $('.content').append($('<p>').text("Max-Temp : " + convertToFahrenheit(data.main.temp_max)));
+    //console.log(data.main.temp);
+  }, (error) => {
+    console.log(error);
+  })
+
+
+
+})
